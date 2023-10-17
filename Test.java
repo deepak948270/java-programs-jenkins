dad5c965c69b154d5a8806883774ec01873d7942
Deepak Sharma <112003902+deepak948270@users.noreply.github.com>
2023-10-17 23:55:50 +0530

    added sysout

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ class Test{
     public static void main(String[] args) {
            System.out.println("testing jenkins ");
            System.out.println("java with jenkins");
+           System.out.println("king thapak");
+
+
     }
 }
